95bf110388b948a7ac3a298d0e43c24f6b8bdd3f
Konstantin Arakantsev <kostya3312@gmail.com>
2020-01-12 23:15:37 +0300

    Add compound styled components

--- src/App.js
+++ src/App.js
@@ -8,7 +8,12 @@ function App() {
     <main>
       {movies.map(movie => (
         <Card key={movie.id} className="mr">
-          {movie.title}
+          <Card.Image src={movie.image} alt={movie.title} />
+          <Card.Body>
+            <Card.Title>{movie.title}</Card.Title>
+            <Card.Text>{movie.desc}</Card.Text>
+            <Card.Button className="111">{movie.ctaText}</Card.Button>
+          </Card.Body>
         </Card>
       ))}
     </main>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,62 @@
 import React from "react";
+import cn from "classnames";
+import { Container, Body, Title, Text, Image, Button } from "./styles/Card";
 
-const Card = ({ children }) => {
-  return <div>{children}</div>;
+const Card = ({ className, classes, children, ...restProps }) => {
+  return (
+    <Container className={cn("card", className, classes)} {...restProps}>
+      {children}
+    </Container>
+  );
+};
+
+Card.Body = ({ className, classes, children, ...restProps }) => {
+  return (
+    <Body className={cn("card__body", className, classes)} {...restProps}>
+      {children}
+    </Body>
+  );
+};
+
+Card.Title = ({ className, classes, children, ...restProps }) => {
+  return (
+    <Title className={cn("card__body", className, classes)} {...restProps}>
+      {children}
+    </Title>
+  );
+};
+
+Card.Text = ({ className, classes, children, ...restProps }) => {
+  return (
+    <Text className={cn("card__text", className, classes)} {...restProps}>
+      {children}
+    </Text>
+  );
+};
+
+Card.Image = ({ src, alt, className, classes, children, ...restProps }) => {
+  return (
+    <Image
+      src={src}
+      alt={alt}
+      className={cn("card__image", className, classes)}
+      {...restProps}
+    >
+      {children}
+    </Image>
+  );
+};
+
+Card.Button = ({ className, classes, children, ...restProps }) => {
+  return (
+    <Button
+      type="button"
+      className={cn("card__button", className, classes)}
+      {...restProps}
+    >
+      {children}
+    </Button>
+  );
 };
 
 export default Card;
